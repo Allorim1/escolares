c9c778841ce3c0f4933c8ee4049d9da8ecf4f0ab
Allorim1 <youselpz@gmail.com>
2026-03-30 14:21:13 -0400

    updated

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/ui/header/header';
 import { Footer } from './shared/ui/footer/footer';
@@ -10,7 +10,18 @@ import { Footer } from './shared/ui/footer/footer';
   templateUrl: './app.html',
   styleUrls: ['./app.css'],
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('escolares');
   showMaintenanceModal = signal(true);
+
+  ngOnInit() {
+    // Auto-dismiss after 3 seconds
+    setTimeout(() => {
+      this.dismissModal();
+    }, 3000);
+  }
+
+  dismissModal() {
+    this.showMaintenanceModal.set(false);
+  }
 }

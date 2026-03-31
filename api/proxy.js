d267a3823e74a570fb2f07745f84667b9033eea2
Allorim1 <youselpz@gmail.com>
2026-03-31 10:28:33 -0400

    updated

--- api/proxy.js
+++ api/proxy.js
@@ -6,8 +6,15 @@ module.exports = async function(req, res) {
     'Content-Type': 'application/json',
   };
 
+  // Pass authorization header
   if (req.headers.authorization) headers['Authorization'] = req.headers.authorization;
+  // Pass DolarVzla API key header
   if (req.headers['x-dolarvzla-key']) headers['x-dolarvzla-key'] = req.headers['x-dolarvzla-key'];
+  // Pass cookies for session management
+  if (req.headers.cookie) headers['Cookie'] = req.headers.cookie;
+  // Pass origin for CORS
+  if (req.headers.origin) headers['Origin'] = req.headers.origin;
+  if (req.headers.referer) headers['Referer'] = req.headers.referer;
 
   try {
     const fetchOptions = {
@@ -20,6 +27,13 @@ module.exports = async function(req, res) {
     }
 
     const response = await fetch(url, fetchOptions);
+    
+    // Pass Set-Cookie headers back to client
+    const setCookieHeader = response.headers.get('set-cookie');
+    if (setCookieHeader) {
+      res.setHeader('Set-Cookie', setCookieHeader);
+    }
+    
     const data = await response.json();
     res.status(response.status).json(data);
   } catch (error) {

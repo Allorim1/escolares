442aa6a5dd120f301454d17a0af513278a384209
Allorim1 <youselpz@gmail.com>
2026-05-22 17:32:58 -0400

    e

--- src/app/admin/noticias/admin-noticias.ts
+++ src/app/admin/noticias/admin-noticias.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { NoticiasService } from '../../shared/data-access/noticias.service';
 import { Noticia } from '../../shared/data-access/noticias.service';
 

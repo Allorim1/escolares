f5ee72a7ddf29c4ab4e1bc11eeb31f41dcbe7689
Allorim1 <youselpz@gmail.com>
2026-05-29 15:49:29 -0400

    Update index.ts

--- src/app/backend/index.ts
+++ src/app/backend/index.ts
@@ -1,5 +1,4 @@
 export * from './models';
-export * from './data-access/mongodb.backend';
 export * from './data-access/marcas.backend';
 export * from './data-access/lineas.backend';
 export * from './data-access/ofertas.backend';

27a1d37d0c75905164f5e6d638392e5845144c9f
Allorim1 <youselpz@gmail.com>
2026-05-29 15:06:39 -0400

    e

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  googleMapsApiKey: process.env['MAPS_API_KEY'],
+  googleMapsApiKey: 'AIzaSyDDb7oaU-zhUqy35AcF_3az7WixJevQEOc',
   apiUrl: '/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  googleMapsApiKey: process.env['MAPS_API_KEY'],
+  googleMapsApiKey: 'AIzaSyDDb7oaU-zhUqy35AcF_3az7WixJevQEOc',
   apiUrl: '/api'
 };

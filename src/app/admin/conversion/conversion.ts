cfc6567f9fb5c0a87759b5e4341776e5ffb369ce
Allorim1 <youselpz@gmail.com>
2026-06-08 17:34:00 -0400

    Update conversion.ts

--- src/app/admin/conversion/conversion.ts
+++ src/app/admin/conversion/conversion.ts
@@ -2261,7 +2261,7 @@ imprimirExpectativas() {
             <div class="print-header">
               <img src="/ESCOLARES%20AZUL%20RIF%20GRANDE.png" class="print-logo" alt="Escolares logo" onerror="this.style.display='none'">
               <div class="print-title-section">
-<h1 style="font-size: 42px; margin: 0;>Metas Ventas ${this.getPeriodoVentasAnterior() ? '- ' + this.getPeriodoVentasAnterior() : ''}</h1>
+<h1 style="font-size: 34px; margin: 0;>Metas Ventas ${this.getPeriodoVentasAnterior() ? '- ' + this.getPeriodoVentasAnterior() : ''}</h1>
                  <div class="meta-info">Período: ${this.getPeriodoVentasAnterior()}</div>
               </div>
             </div>

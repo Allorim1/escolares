7117ab70b6887382578e9919842ec7d7f539dea7
Allorim1 <youselpz@gmail.com>
2026-06-18 16:58:07 -0400

    e

--- src/app/shared/services/google-maps.service.ts
+++ src/app/shared/services/google-maps.service.ts
@@ -25,7 +25,7 @@ async loadApi(): Promise<void> {
      
      return new Promise((resolve, reject) => {
        const script = document.createElement('script');
-       script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places,directions,marker`;
+       script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places`;
        script.async = true;
        script.defer = true;
        script.onload = () => {

cd0ee0d76d2a716a49d41e667e5ae2f06588fa2c
Allorim1 <youselpz@gmail.com>
2026-07-10 17:01:36 -0400

    e

--- src/app/lineas/lineas.ts
+++ src/app/lineas/lineas.ts
@@ -6,10 +6,12 @@ import {
   QueryList,
   inject,
   PLATFORM_ID,
+  signal,
 } from '@angular/core';
 import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { LineasService } from '../shared/data-access/lineas.service';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-lineas',
@@ -20,10 +22,20 @@ import { LineasService } from '../shared/data-access/lineas.service';
 export class Lineas implements AfterViewInit {
   lineasService = inject(LineasService);
   lineas = this.lineasService.lineas;
+  private router = inject(Router);
+  private route = inject(ActivatedRoute);
+
+  showGrid = signal(true);
 
   @ViewChildren('revealElement') revealElements!: QueryList<ElementRef>;
 
   constructor() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.showGrid.set(this.route.firstChild === null);
+      }
+    });
+
     if (isPlatformBrowser(inject(PLATFORM_ID))) {
       setTimeout(() => this.revealAll(), 100);
     }
